633ab449e0772a44773c0840bb8cefa3c53dcc10
HuhYujin <yujin4802@naver.com>
2023-07-06 14:53:34 +0900

    커밋4 나누기 추가

--- main.py
+++ main.py
@@ -8,6 +8,9 @@ def sub_func(n1,n2):
 def mul_func(n1,n2):
     return n1*n2
 
+def div_func(n1,n2):
+    return n1/n2
+
 
 
 ##전역변수부
@@ -23,4 +26,7 @@ print(num1,"-",num2,"=",result)
 
 result=mul_func(num1,num2)
 print(num1,"*",num2,"=",result)
+
+result=div_func(num1,num2)
+print(num1,"/",num2,"=",result)
 
